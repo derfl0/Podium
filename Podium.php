bf9840cf2e041c3211a7a7d7a042a32b9da48980
Florian Bieringer <florian.bieringer@uni-passau.de>
2015-11-19 13:15:20 +0100

    SemTree und Einrichtungen

--- Podium.php
+++ Podium.php
@@ -1,14 +1,15 @@
 <?php
 
 /**
- * QuickfilePlugin.class.php
+ * Podium.php
+ *
+ * Quickaccess anything
  *
  * @author  Florian Bieringer <florian.bieringer@uni-passau.de>
  */
 class Podium extends StudIPPlugin implements SystemPlugin
 {
-
-    public static $types = array();
+    private static $types = array();
 
     public function __construct()
     {
@@ -27,15 +28,39 @@ class Podium extends StudIPPlugin implements SystemPlugin
         self::addType('courses', _('Veranstaltungen'), array($this, 'search_course'), array($this, 'filter_course'));
         self::addType('user', _('Benutzer'), array($this, 'search_user'), array($this, 'filter_user'));
         self::addType('file', _('Datei'), array($this, 'search_files'), array($this, 'filter_file'));
+        self::addType('inst', _('Einrichtungen'), array($this, 'search_inst'), array($this, 'filter_inst'));
+        self::addType('semtree', _('Studienbereiche'), array($this, 'search_semtree'), array($this, 'filter_semtree'));
     }
 
-    public static function addType($index, $name, $sql, $filter) {
+    /**
+     * Add a type to podium
+     *
+     * @param $index string typeindexname
+     * @param $name string typename
+     * @param $sql function Callback to retrieve the sql
+     * @param $filter function Callback to get array formated result
+     */
+    public static function addType($index, $name, $sql, $filter)
+    {
         self::$types[$index] = array(
             'name' => $name,
             'sql' => $sql,
             'filter' => $filter);
     }
 
+    /**
+     * Removes a type from podium
+     *
+     * @param $index typeindexname
+     */
+    public static function removeType($index)
+    {
+        unset(self::$types[$index]);
+    }
+
+    /**
+     * Kickoff function to start query
+     */
     public function find_action()
     {
         $types = self::$types;
@@ -112,13 +137,11 @@ class Podium extends StudIPPlugin implements SystemPlugin
         return $string;
     }
 
-    /**
-     * Returns SQL for the file search
-     *
-     * @param $search
-     * @return string
+    /*
+     * ###DEFAULT FUNCTIONS!
      */
-    public function search_files($search)
+
+    private function search_files($search)
     {
         // Filter for own courses
         if (!$GLOBALS['perm']->have_perm('admin')) {
@@ -151,7 +174,7 @@ class Podium extends StudIPPlugin implements SystemPlugin
         return $sql;
     }
 
-    public function filter_file($file_id, $search)
+    private function filter_file($file_id, $search)
     {
         $file = StudipDocument::find($file_id);
         if ($file->checkAccess(User::findCurrent()->id)) {
@@ -165,7 +188,7 @@ class Podium extends StudIPPlugin implements SystemPlugin
         }
     }
 
-    public function search_user($search)
+    private function search_user($search)
     {
         if (!$search) {
             return null;
@@ -176,7 +199,7 @@ class Podium extends StudIPPlugin implements SystemPlugin
         return $sql;
     }
 
-    public function filter_user($user_id, $search)
+    private function filter_user($user_id, $search)
     {
         $user = User::find($user_id);
         return array(
@@ -188,7 +211,7 @@ class Podium extends StudIPPlugin implements SystemPlugin
         );
     }
 
-    public function search_mycourse($search)
+    private function search_mycourse($search)
     {
         if (!$search) {
             return null;
@@ -199,7 +222,7 @@ class Podium extends StudIPPlugin implements SystemPlugin
         return $sql;
     }
 
-    public function search_course($search)
+    private function search_course($search)
     {
         if (!$search) {
             return null;
@@ -215,7 +238,7 @@ class Podium extends StudIPPlugin implements SystemPlugin
         return $sql;
     }
 
-    public function filter_course($course_id, $search)
+    private function filter_course($course_id, $search)
     {
         $course = Course::find($course_id);
         return array(
@@ -224,6 +247,46 @@ class Podium extends StudIPPlugin implements SystemPlugin
             'url' => URLHelper::getURL("dispatch.php/course/details", array('cid' => $course->id)),
             'img' => CourseAvatar::getAvatar($course->id)->getUrl(AVATAR::MEDIUM),
             'date' => $course->start_semester->name
+        );
+    }
+
+    private function search_semtree($search)
+    {
+        if (!$search) {
+            return null;
+        }
+        $query = DBManager::get()->quote("%$search%");
+        $sql = "SELECT 'semtree' as type, sem_tree_id as id FROM sem_tree WHERE name LIKE $query ORDER BY name DESC";
+        return $sql;
+    }
+
+    private function filter_semtree($semtree_id, $search)
+    {
+        $semtree = StudipStudyArea::find($semtree_id);
+        return array(
+            'id' => $semtree->id,
+            'name' => self::mark($semtree->name, $search),
+            'url' => URLHelper::getURL("dispatch.php/search/courses", array('start_item_id' => $semtree->id, 'level' => 'vv', 'cmd' => 'qs'))
+        );
+    }
+
+    private function search_inst($search)
+    {
+        if (!$search) {
+            return null;
+        }
+        $query = DBManager::get()->quote("%$search%");
+        $sql = "SELECT 'inst' as type, Institut_id as id FROM Institute WHERE Name LIKE $query ORDER BY name DESC";
+        return $sql;
+    }
+
+    private function filter_inst($inst_id, $search)
+    {
+        $inst = Institute::find($inst_id);
+        return array(
+            'id' => $inst->id,
+            'name' => self::mark($inst->getFullname(), $search),
+            'url' => URLHelper::getURL("dispatch.php/institute/overview", array('cid' => $inst->id))
         );
     }
 
